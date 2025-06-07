d0e8febc3c5937ae2d8c4ad8582f0ab9bac8ef83
Jitpanoo Polawath <jitpanoopolawath@gmail.com>
2025-06-07 20:20:57 +0700

    Added parallelism

--- atariDataset.py
+++ atariDataset.py
@@ -2,31 +2,61 @@ import numpy as np
 from tqdm import tqdm
 import gymnasium as gym
 import ale_py
+import os
+import gc
+from multiprocessing import Pool, TimeoutError
 
-# Initialize atari 
-gym.register_envs(ale_py)
 
 # PARAMTER
 NUM_STEPS = 1000
-NUM_EPS = 1000
+NUM_EPS = 100
 
 # Save image numpy
 def save_numpy(filename, list_of_nump):
     with open (f"{filename}.npy", "wb") as f:
         np.save(f, list_of_nump)
 
-# Loop through 3 envs
-for env_name in ['Breakout-v5','Assault-v5', 'Pong-v5']:
+
+def env_data(env_name):
     print(f"============ Creating: {env_name} dataset =============")
-    env = gym.make(f'ALE/{env_name}')
-    list_of_nump = []
+    env = gym.make(f'ALE/{env_name}')    
+    obs, info = env.reset()
+    obs_shape = obs.shape
+    max_observations = NUM_EPS * NUM_STEPS
+    temp_filename = f"{env_name}_temp.npy"
+    
+    memmap_array = np.memmap(temp_filename, dtype=obs.dtype, mode='w+', 
+                            shape=(max_observations,) + obs_shape)
+    
+    observation_count = 0
+    
     for episode in tqdm(range(NUM_EPS)):
         obs, info = env.reset()
-        for step in range(NUM_STEPS):            
+        
+        for step in range(NUM_STEPS):
             obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
-            list_of_nump.append(obs)
+            memmap_array[observation_count] = obs
+            observation_count += 1
+            
             if terminated or truncated:
                 break
-    nump_list_nump = np.array(list_of_nump)
-    save_numpy(f"{env_name}", nump_list_nump)
+    
+    final_array = memmap_array[:observation_count]
+    np.save(f"{env_name}.npy", final_array)
+    
+    del final_array
+    del memmap_array
+    os.remove(temp_filename)
     env.close()
+
+
+
+if __name__=="__main__":
+
+    # Loop through 3 envs
+    with Pool(processes=3) as pool:
+        pool.map(env_data,['Breakout-v5', 'Assault-v5', 'Pong-v5'])
+
+        # Clean up temp files after all processing is done
+        print("Cleaning up temporary files...")
+        gc.collect()
